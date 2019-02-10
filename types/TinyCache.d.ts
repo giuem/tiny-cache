0cd3564f0de50acb0fd906870de3f7e2b46991ba
giuem <giuemcom@gmail.com>
2019-02-10 21:40:54 +0800

    fix: update types

--- types/TinyCache.d.ts
+++ types/TinyCache.d.ts
@@ -2,7 +2,7 @@ import { ICallback, IScriptConfig, ITinyCacheConfig } from "./types";
 export declare class TinyCache {
     private config;
     constructor(config?: ITinyCacheConfig);
-    load(scripts: IScriptConfig[]): Promise<void>;
+    load(scripts: IScriptConfig[]): Promise<void> | void;
     load(scripts: IScriptConfig[], callback: ICallback<Array<null | undefined>>): void;
     remove(script: IScriptConfig): void;
     private loadScript;
