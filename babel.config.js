c694b8d6b54a22b103e2062a19cd369985c502db
giuem <giuemcom@gmail.com>
2019-03-16 12:13:16 +0800

    chore(deps): remove unused babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 const presets = ["@babel/env", "@babel/preset-typescript"];
 
 const plugins = [
-  ["@babel/proposal-class-properties", { loose: true }],
-  "@babel/proposal-object-rest-spread",
   ["@babel/plugin-transform-template-literals", { loose: true }],
-  ["@babel/plugin-transform-classes", { loose: true }]
 ];
 
 module.exports = { presets, plugins };
